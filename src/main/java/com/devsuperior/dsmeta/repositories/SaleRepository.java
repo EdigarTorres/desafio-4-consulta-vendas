8eb38b84353d24088ca8066726415220420e15d0
Edigar <edigartorres7@gmail.com>
2026-05-04 08:19:14 -0300

    Feat: implementação das buscas em SaleRepository

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,32 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SellerMinDTO;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.awt.print.Pageable;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleMinDTO(obj.id, obj.date, obj.amount, obj.seller.name) "
+            + "FROM Sale obj JOIN obj.seller s "
+            + "WHERE obj.date BETWEEN :minDate AND :maxDate AND UPPER(s.name) LIKE UPPER(CONCAT('%', :sellerName, '%'))")
+    Page<SaleMinDTO> findByDateBetweenAndSellerName(LocalDate minDate, LocalDate maxDate, String sellerName, Pageable pageable);
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SellerMinDto(s.seller.name AS sellerName, SUM(s.amount) AS totalAmount) " +
+            "FROM Sale s " +
+            "WHERE s.date BETWEEN :minDate AND :maxDate " +
+            "GROUP BY s.seller.id, s.seller.name")
+    List<SellerMinDTO> findByProjection2(
+            @Param("minDate") LocalDate minDate,
+            @Param("maxDate") LocalDate maxDate);
+
 
 }
